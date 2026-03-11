8075a8c62d31cd42f54099bed9605904b155d2f4
“pepito <vmartinez@alumno.huergo.edu.ar>
2026-03-11 10:51:06 -0300

    clase

---
+++ aba/9.1.py
@@ -0,0 +1,23 @@
+import time
+lugares = ["Puerto Rico", "Madrid", "París", "Londres", "Nueva York"]
+def main():
+    imprimir_lugares()
+
+    
+def imprimir_lugares():
+    print(lugares)
+    time.sleep(1)
+    print(f"la lista ordenada es: {sorted(lugares)}")
+    time.sleep(1)
+    print(lugares)
+    time.sleep(1)
+    print(f"la lista ordenada al revés es: {sorted(lugares, reverse=True)}")
+    time.sleep(1)
+    print(lugares)
+    time.sleep(1)
+    print(list(reversed(lugares)))
+    time.sleep(1)
+    print(list(reversed(lugares)))
+
+
+main()
